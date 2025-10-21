df40dd625574dd2310aaef41f8ec3d56e7438ba4
Kayky <al.kayky.martins@impatech.edu.br>
2025-10-21 18:48:22 -0300

    adicionando a 1a versão do erros.py

--- erros.py
+++ erros.py
@@ -1,1 +1,29 @@
-print('test')
+def erro_absoluto(valor_real,valor_aprox):
+    """
+    Calcula o erro absoluto entre o valor real/definido/sla e o valor aproximado.
+
+    Parâmetros:
+        valor_real(float): Valor considerado verdadeiro.
+        valor_aprox(float): Valor aproximado
+
+    Retorna:
+        float: O erro absoluto, dado por |valor_real - valor_aprox|.
+    """
+    return abs(valor_real-valor_aprox)
+
+def erro_relativo(valor_real,valor_aprox):
+    """
+    Calcula o erro relativo entre o valor real e o valor aproximado.
+
+    Parâmetros:
+        valor_real(float): Valor considerado verdadeiro.
+        valor_aprox(float): Valor aproximado.
+
+    Retorna:
+        float: O erro relativo, dado por |valor_real - valor_aprox| / |valor_real|.
+    """
+    return abs(valor_real-valor_aprox)/abs(valor_real)
+a=float(input("Digite um valor: "))
+b=float(input("Digite um valor aproximado para o valor anterior: "))
+print(f"Erro absoluto: {erro_absoluto(a,b):.6f}")
+print(f"Erro relativo: {erro_relativo(a,b):.8f}")
