d45e76882bbddcc7c06f5d7dd5830865437c798f
Mateus-Band <al.mateus.torres@impatech.edu.br>
2025-11-19 12:54:02 -0300

    Ajuste de arquivo para a prova

--- sandbox/teste_prova.py
+++ sandbox/teste_prova.py
@@ -1,1 +1,9 @@
 #Arquivo para dia da aplicação
+
+#ajuste de caminho para importação do projeto.
+import sys
+import os
+notebook_dir = os.getcwd()
+project_root = os.path.abspath(os.path.join(notebook_dir, '..'))
+if project_root not in sys.path:
+    sys.path.append(project_root)
